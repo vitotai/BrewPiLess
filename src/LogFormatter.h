7f1d8c202fd089dc41989385f7b14e75867ec1f1
Vito Tai <vitotai@mail.com>
2019-06-29 07:50:24 +0800

    Split logging functions

---
+++ src/LogFormatter.h
@@ -0,0 +1,22 @@
+#ifndef LogFormatter_H
+#define LogFormatter_H
+
+#define KeyBeerTemp "beerTemp"
+#define KeyBeerSet "beerSet"
+#define KeyFridgeTemp "fridgeTemp"
+#define KeyFridgeSet "fridgeSet"
+#define KeyRoomTemp "roomTemp"
+#define KeyGravity "gravity"
+#define KeyPlato "plato"
+#define KeyAuxTemp "auxTemp"
+#define KeyVoltage "voltage"
+#define KeyTilt  "tilt"
+#define KeyPressure "pressure"
+#define KeyMode "mode"
+
+
+size_t nonNullJson(char *buffer,size_t size);
+size_t dataSprintf(char *buffer,const char *format,const char* invalidStr);
+
+
+#endif
