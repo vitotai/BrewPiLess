3ca49636e350846539ebd5260495847118849f60
Vito Tai <vitotai@mail.com>
2019-09-24 10:38:46 +0800

    ESP32 HTTP Update Server

---
+++ src/ESP32HTTPUpdateServer.cpp
@@ -0,0 +1,46 @@
+
+#include "ESP32HTTPUpdateServer.h"
+
+static const char* IndexPage = R"END(
+<html><body><form method='POST' action='' enctype='multipart/form-data'>
+<input type='file' name='update'>
+<input type='submit' value='Update'>
+</form></body></html>
+)END";
+
+void ESP32HTTPUpdateServer::setup(WebServer& server,String servicePath,const char* user,const char* pass) {
+
+    server.on(servicePath, HTTP_GET, [&]() {
+     	if(user != NULL && pass != NULL && !server.authenticate(user, pass))
+ 	    	return server.requestAuthentication();
+        server.sendHeader("Connection", "close");
+        server.send(200, "text/html", IndexPage);
+    });
+    
+    server.on("/update", HTTP_POST, [&]() {
+        server.sendHeader("Connection", "close");
+        if(Update.hasError())
+            server.send(501, "text/plain", String("Update Failed:") + String(Update.getError()));
+        else
+            server.send(200, "text/plain", "Update Successfull!");
+    }, [&]() {
+        HTTPUpload& upload = server.upload();
+        if (upload.status == UPLOAD_FILE_START) {
+            Serial.printf("Update: %s\n", upload.filename.c_str());
+            if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+                //Update.printError(Serial);
+            }
+        } else if (upload.status == UPLOAD_FILE_WRITE) {
+            /* flashing firmware to ESP*/
+            if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+                //Update.printError(Serial);
+            }
+        } else if (upload.status == UPLOAD_FILE_END) {
+            if (Update.end(true)) { //true to set the size to the current progress
+                //Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+            } else {
+                //Update.printError(Serial);
+            }
+        }
+    });
+}
