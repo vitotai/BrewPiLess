be31698b9c891c11ddaee76ebf420edddb21e6c0
Vito Tai <vitotai@mail.com>
2024-02-18 15:50:19 +0800

    split errorcode to another file

---
+++ src/ErrorCode.h
@@ -0,0 +1,14 @@
+#ifndef ERRORCODE_H
+#define ERRORCODE_H
+
+#define ErrorNone 0
+
+#define ErrorLogNotEnoughSpace  101
+#define ErrorLogFailedToCreateDirectory 102
+#define ErrorLogFailedOpenFile 103
+#define ErrorLogFailedResumeOpenFile 104 
+#define ErrorLogFailedResumeReadFile 105 
+#define ErrorLogFailedResumeInvalidFormat 106
+#define ErrorLogFailedResumeOpenForWrite 107
+
+#endif
