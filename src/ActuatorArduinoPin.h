f4ecfd8f03fefdd9175e898c0e3e6f27d1e07995
Jon <jonathan.schneider@gmx.de>
2024-10-28 22:02:03 -0500

    proof of concept

--- src/ActuatorArduinoPin.h
+++ src/ActuatorArduinoPin.h
@@ -9,6 +9,10 @@
 
 #include "Actuator.h"
 
+#ifndef LATCHING_RELAYS
+#define LATCHING_RELAYS false
+#endif
+
 template<uint8_t pin, bool invert>
 class DigitalConstantPinActuator ACTUATOR_BASE_CLASS_DECL
 {
@@ -47,7 +51,20 @@ class DigitalPinActuator ACTUATOR_BASE_CLASS_DECL
 
 	inline ACTUATOR_METHOD void setActive(bool active) {
 		this->active = active;
-		digitalWrite(pin, active^invert ? HIGH : LOW);
+
+		#if LATCHING_RELAYS // Latching relay
+			if (active) {
+				digitalWrite(LATCH_PIN_SET, HIGH);
+				delay(10);
+				digitalWrite(LATCH_PIN_SET, LOW);
+			} else {
+				digitalWrite(LATCH_PIN_RESET, HIGH);
+				delay(10);
+				digitalWrite(LATCH_PIN_RESET, LOW);
+			}
+		#else
+			digitalWrite(pin, active^invert ? HIGH : LOW);
+		#endif
 	}
 
 	bool isActive() { return active; }
